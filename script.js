cd772ee137b432576a360853215635eebf89acf2
Çağlar <2038887+ycaglar@users.noreply.github.com>
2024-03-27 21:15:43 -0500

    Implement random coloring on grids

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ let container = document.querySelector('.container');
 
 form_grid(16);
 
+function generate_random_color() {
+  let red = Math.floor(Math.random() * 256);
+  let green = Math.floor(Math.random() * 256);
+  let blue = Math.floor(Math.random() * 256);
+  return `rgb(${red}, ${green}, ${blue})`
+};
+
 function form_grid(grid_size) {
   grid_size = Math.min(grid_size, 100);
   for (let i = 0; i < grid_size ** 2; ++i) {
@@ -9,7 +16,7 @@ function form_grid(grid_size) {
     square.style.flex = `0 0 calc(960px / ${grid_size})`;
     square.style.height = `calc(960 / ${grid_size})px`
     square.addEventListener('mouseover', function (event) {
-      square.style.backgroundColor = 'pink';
+      square.style.backgroundColor = generate_random_color();
     });
     square.classList.add('square');
     container.appendChild(square);
